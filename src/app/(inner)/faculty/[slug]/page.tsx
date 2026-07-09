07a9b751004387ebf02bd6bf6e7fe2a151b459c9
sandeepsterco <sandeep.rawat@stercodigitex.com>
2026-07-09 18:09:15 +0530

    fix faculty detail page

--- src/app/(inner)/faculty/[slug]/page.tsx
+++ src/app/(inner)/faculty/[slug]/page.tsx
@@ -4,17 +4,28 @@ import apiFetch from "@/lib/api"
 import { getSlug } from "@/lib/getSlug"
 import { notFound } from "next/navigation";
 
-export default async function FacultyDetail(){
-    const slug = await getSlug();
-    const {data, error} = await apiFetch(`faculty/${slug}`)
+interface SearchParams {
+    search?: string;
+    duration?: string;
+    school?: string;
+}
 
-    if(error || !data.status){
+interface FacultyDetailProps {
+    searchParams: Promise<SearchParams>;
+}
+
+export default async function FacultyDetail({ searchParams }: FacultyDetailProps) {
+    const slug = await getSlug();
+    const resolvedSearchParams = await searchParams;
+    const { data, error } = await apiFetch(`faculty/${slug}`);
+
+    if (error || !data.status) {
         notFound();
     }
 
     const combinedHTML = Object.values(data?.faculty?.cms).join('') || '';
 
-    return(
-        <ReactParserDynamic html={combinedHTML} />
-    )
+    return (
+        <ReactParserDynamic html={combinedHTML} searchParams={resolvedSearchParams} />
+    );
 }
